92d42a149572220c27aaa66a103348e799308bf2
Adpiosk2010 <147307196+Adpiosk2010@users.noreply.github.com>
2023-10-08 11:13:22 +0200

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            }
+word = input("SHEESH): ")
+            
+if word in meme_dict.keys():
+    print(name_dict[word])
+else:
+    print("lekka dezaprobata")
